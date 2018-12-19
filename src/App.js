5020854e83a23d2bbe073f30623fd4d82b847279
Cristian Marquez <xthecapx@gmail.com>
2018-12-19 16:15:39 -0500

    setState on click

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   state = {
     persons: [
       {
-        name: 'Cristian', age: 28
+        name: "Cristian",
+        age: 28
       },
       {
-        name: 'Daniel', age: 29
+        name: "Daniel",
+        age: 29
       },
       {
-        name: 'Marquez', age: 30
+        name: "Marquez",
+        age: 30
       }
-    ]
+    ],
+    otherState: "Some other vale"
+  };
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        {
+          name: "Cristian New",
+          age: 28
+        },
+        {
+          name: "Daniel",
+          age: 29
+        },
+        {
+          name: "Marquez",
+          age: 30
+        }
+      ]
+    });
   };
 
   render() {
@@ -23,9 +46,11 @@ class App extends Component {
       <div className="App">
         <h1>Hello</h1>
         <p>This is working!</p>
-        <button>Switch name</button>
+        <button onClick={this.switchNameHandler}>Switch name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+          My Hobbies: Racing
+        </Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
