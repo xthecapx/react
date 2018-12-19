029a06a2aa0d3b50652ecf223816ff8e8190ecbb
Cristian Marquez <xthecapx@gmail.com>
2018-12-19 15:44:32 -0500

    Adding js code in jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <h1>Hello</h1>
         <p>This is working!</p>
+        <Person />
       </div>
     );
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hello'))
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Person() {
+    return <p>I'm a person {Math.floor(Math.random() * 30)}</p>
+}
+
+export default Person;
