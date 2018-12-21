7b18d969475c167b972cf30ce7acf0588def3042
Cristian Marquez <xthecapx@gmail.com>
2018-12-21 08:25:57 -0500

    Added inline styles and webpack styling

--- src/App.js
+++ src/App.js
@@ -61,12 +61,22 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hello</h1>
         <p>This is working!</p>
         {/** slow code */}
-        <button onClick={() => this.switchNameHandler('Cris*')}>Switch name</button>
+        <button 
+          style={style}
+          onClick={() => this.switchNameHandler('Cris*')}>Switch name</button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age} />
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,10 @@
 import React from 'react';
+// Webpack handle this import
+import './Person.css';
 
 function Person(props) {
     return (
-        <div>
+        <div className="Person">
             <p>I'm {props.name} and I am {props.age} years old!</p>
             <p onClick={props.click}>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
