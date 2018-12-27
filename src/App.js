aef2a4e85e773b33ac12734c495f44c07a8759bd
Cristian Marquez <xthecapx@gmail.com>
2018-12-27 12:02:31 -0500

    Refactor to use css modules

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
+// https://github.com/css-modules/css-modules
 class App extends Component {
   state = {
     persons: [
@@ -96,20 +97,20 @@ class App extends Component {
     }
 
     // Dynamic classes
-    const classes = [];
+    const assignedClasses = [];
 
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
 
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     return (
-      <div className='App'>
+      <div className={classes.App}>
         <h1>Hello</h1>
-        <p className={classes.join(' ')}> This is working!</p>
+        <p className={assignedClasses.join(' ')}> This is working!</p>
         {/** slow code */}
         <button style={style} onClick={this.togglePersonsHandler}>
           Toggle Person
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,10 @@
 import React from 'react';
 // Webpack handle this import
-import './Person.css';
+import classes from './Person.css';
 
 function Person(props) {
   return (
-    <div className='Person' style={style}>
+    <div className={classes.Person}>
       <p onClick={props.click}>
         I'm {props.name} and I am {props.age} years old!
       </p>
