36cb067d8da95b4f5790ede3688d023079468d32
Cristian Marquez <xthecapx@gmail.com>
2019-01-02 10:02:54 -0500

    from stateless to statefull

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,16 +2,18 @@ import React from 'react';
 // Webpack handle this import
 import classes from './Person.css';
 
-function Person(props) {
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.click}>
-        I'm {props.name} and I am {props.age} years old!
-      </p>
-      <p>{props.children}</p>
-      <input type='text' onChange={props.changed} value={props.name} />
-    </div>
-  );
+class Person extends React.Component {
+  render() {
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.click}>
+          I'm {this.props.name} and I am {this.props.age} years old!
+        </p>
+        <p>{this.props.children}</p>
+        <input type='text' onChange={this.props.changed} value={this.props.name} />
+      </div>
+    );
+  }
 }
 
 export default Person;
